3c4a8407b426278b0170aad88a5490a9c17fb536
Emad Anwer <emadanwer.official@gmail.com>
2024-01-08 14:21:00 +0200

    [add] task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+"""Lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened.
+    Args:
+        boxes: list of lists
+    Returns:
+        True if all boxes can be opened, else return False
+    """
+    keys = set()
+    keys.add(0)
+    for i in range(len(boxes)):
+        for key in boxes[i]:
+            if (key > i or key < i):
+                keys.add(key)
+    return len(boxes) == len(keys)
