e2d300eb0162bc1fa87236c3a04e201efac0729a
Emad Anwer <emadanwer.official@gmail.com>
2024-01-09 19:37:07 +0200

    [fix] using depth-first search (DFS)

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,10 +9,20 @@ def canUnlockAll(boxes):
     Returns:
         True if all boxes can be opened, else return False
     """
-    keys = set()
-    keys.add(0)
-    for i in range(len(boxes)):
-        for key in boxes[i]:
-            if (key > i or key < i):
-                keys.add(key)
-    return len(boxes) == len(keys)
+    if not boxes or not boxes[0]:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited)
