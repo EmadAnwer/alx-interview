94038d62502c48a246937d234697726638f3e2e3
EmadAnwer <emadanwer.official@gmail.com>
2024-02-01 18:38:01 +0200

    [try]

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,22 +5,18 @@
 
 
 def validUTF8(data):
-    """determines if a given data set represents a valid UTF-8 encoding."""
-
     def byte_sequence_count(byte):
-        """return numbers of bytes and validate the byte"""
-
-        binary_representation = bin(byte)[2:].rjust(8, '0') 
-        if binary_representation[:2] == "10":
+        binary_representation = bin(byte)[2:].rjust(
+            8, "0"
+        )  # Pad with zeros to get 8 bits
+        if binary_representation.startswith("0"):
             return 1
-        elif binary_representation[:3] == "110":
+        elif binary_representation.startswith("110"):
             return 2
-        elif binary_representation[:4] == "1110":
+        elif binary_representation.startswith("1110"):
             return 3
-        elif binary_representation[:5] == "11110":
+        elif binary_representation.startswith("11110"):
             return 4
-        elif binary_representation[:1] == "0":
-            return 0
         else:
             return -1
 
@@ -32,22 +28,16 @@ def validUTF8(data):
             return False
 
         i += 1
-        if sequence_count == 0:
-            continue
-
         if sequence_count == 1:
-            return False
+            continue
 
         if i + sequence_count - 1 > len(data):
             return False
 
-        j = 0
-        while j < sequence_count - 1:
-            if byte_sequence_count(data[i + j]) != 1:
+        for j in range(sequence_count - 1):
+            if not (data[i + j] & 0b11000000 == 0b10000000):
                 return False
 
-            j += 1
-
-        i += sequence_count
+        i += sequence_count - 1
 
     return True
