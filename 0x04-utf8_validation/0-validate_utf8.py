88490dd4f658a5b3c448626ab82c5bded7a68b5e
EmadAnwer <emadanwer.official@gmail.com>
2024-02-01 23:13:09 +0200

    [test]

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,36 +9,31 @@ def validUTF8(data) -> bool:
 
     def byte_sequence_count(byte):
         """Returns the number of bytes in a UTF-8 sequence"""
-        binary_representation = bin(byte)[2:].rjust(8, "0")
-        if binary_representation.startswith("0"):
-            return 1
-        elif binary_representation.startswith("110"):
-            return 2
-        elif binary_representation.startswith("1110"):
-            return 3
-        elif binary_representation.startswith("11110"):
-            return 4
-        else:
-            return -1
+        leading_ones = 0
+        while (byte >> 7 - leading_ones) & 1:
+            leading_ones += 1
+        return leading_ones
 
     i = 0
-
+    # 11000010 10000000 11000010 10000000
+    # 01000010
     while i < len(data):
         sequence_count = byte_sequence_count(data[i])
-        if sequence_count == -1:
-            return False
 
-        i += 1
-        if sequence_count == 1:
+        if sequence_count == 0:
+            i += 1
             continue
 
-        if i + sequence_count - 1 > len(data):
+        if sequence_count == 1 or sequence_count > 4:
             return False
 
-        for j in range(sequence_count - 1):
+        if i + sequence_count > len(data):
+            return False
+        # 11000010 10000000
+        for j in range(1, sequence_count):
             if not (data[i + j] >> 6 == 0b10):
                 return False
 
-        i += sequence_count - 1
+        i += sequence_count
 
     return True
