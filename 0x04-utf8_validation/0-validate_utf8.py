c72d6248d6335d214bf634c68969af1723740655
EmadAnwer <emadanwer.official@gmail.com>
2024-02-01 14:54:09 +0200

    [task 0]

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+0-validate_utf8 module
+"""
+
+
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding."""
+
+    def byte_sequence_count(byte):
+        """retun numbers of bytes and validate the byte"""
+
+        binary_represenstaion = bin(byte)[2:]
+        if len(binary_represenstaion) > 8:
+            return -1
+
+        if len(binary_represenstaion) < 8:
+            return 0
+
+        if binary_represenstaion[:2] == "10":
+            return 1
+
+        sequence_count = len(binary_represenstaion[:5].split("0")[0])
+        if sequence_count == 5:
+            return -1
+
+        return sequence_count
+
+    i = 0
+
+    while i < len(data):
+        sequence_count = byte_sequence_count(data[i])
+        if sequence_count == -1:
+            return False
+
+        if sequence_count == 0:
+            i += 1
+            continue
+
+        for j in range(i + 1, i + sequence_count + 1):
+            if j >= len(data):
+                return False
+
+            if byte_sequence_count(data[j]) != 1:
+                return False
+
+        i += sequence_count + 1
+
+    return True
