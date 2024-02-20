8dfa698019eda9c041d2c640cae93f46886ef7d2
EmadAnwer <emadanwer.official@gmail.com>
2024-02-20 23:46:29 +0200

    [test]

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Rotate a 2D matrix by 90 degrees"""
+
+
+def rotate_2d_matrix(matrix: list):
+    """Rotate a 2D matrix by 90 degrees"""
+    length = len(matrix[0])
+    matrix.extend([[] for _ in range(length)])
+
+    for i in range(len(matrix), len(matrix) - length, -1):
+        for j in range(len(matrix) - length, 0, -1):
+            print(i, j)
+            matrix[i - 1].append(matrix[j - 1].pop())
+    del matrix[0 : len(matrix) - length]
+
+
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# rotate_2d_matrix(matrix=matrix)
+# print(matrix)
