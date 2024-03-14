39e3e4fba76bd16aeb328bd63c5bd73da24c6ae7
EmadAnwer <emadanwer.official@gmail.com>
2024-03-14 14:58:37 +0200

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
+"""
+Prime Game Module
+"""
 
 
 def isWinner(x, nums):
+    """
+    Prime Game Function
+    """
     maria = 0
     ben = 0
 
     def SieveOfEratosthenes(n):
+        """Sieve of Eratosthenes Function"""
+
         prime = [True for i in range(n + 1)]
-        p = 2
-        while p * p <= n:
+        pointer = 2
+        while pointer * pointer <= n:
             if prime[p] == True:
-                for i in range(p * p, n + 1, p):
+                for i in range(pointer * pointer, n + 1, pointer):
                     prime[i] = False
-            p += 1
+            pointer += 1
         prime[0] = False
         prime[1] = False
         return prime
